4b093a4999f28b19fe6a292dbc038f6ec026cc67
DDRC <ddc.rivas@yavirac.edu.ec>
2020-06-25 14:32:43 -0500

    calculo de edad con c#
    este es el primer intento que he hecho para crear una aplicacion de consola con los metodos de lectura y escritura en consola

---
+++ AppDeConsola/AppDeConsola/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace AppDeConsolaEdad
+{
+    class Edadpersona
+    {
+        static void Main(string[] Datos)
+        {
+            if (Datos.Length > 0 && Datos.Length==2)
+            {
+                int añoActual = 2020;
+                Console.WriteLine("Hola " + Datos[0]+" "+Datos[1]);
+                Console.Write("Por favor ingresa tu año de nacimiento:");
+                String fecNac = Console.ReadLine();
+                int añoNacimiento = int.Parse(fecNac);
+                int edad = añoActual - añoNacimiento;
+                Console.Write("Vaya al parecer tu edad es " + edad);
+                Console.WriteLine(" Años !!");
+                Console.WriteLine("Presione Enter para salir");
+                Console.ReadKey();
+            }
+            else {
+                Console.WriteLine("Debe ingresar su nombre y apellido, para continuar con el proceso");
+                Console.WriteLine("Presione Enter para salir");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
